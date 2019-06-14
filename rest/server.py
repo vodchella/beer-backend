8ca4e9aca4b97ea5b4e7fa87e400686da1423e92
vodchella <twister.kz@gmail.com>
2019-06-14 15:07:43 +0600

    Добавил номер порта в pid-файл

--- rest/server.py
+++ rest/server.py
@@ -19,21 +19,22 @@ if __name__ == '__main__':
     if sys.version_info < (3, 6):
         panic('We need mininum Python verion 3.6 to run. Current version: %s.%s.%s' % sys.version_info[:3])
 
-    pid_file = 'beer-rest'
+    try:
+        host = CONFIG['rest']['listen_host']
+        port = CONFIG['rest']['listen_port']
+    except:
+        DEFAULT_LOGGER.critical(get_raised_error(full=True))
+        sys.exit(1)
+
+    pid_file = f'beer-rest-{port}'
     pid_dir = tempfile.gettempdir()
+    pid_file_full = f'{pid_dir}/{pid_file}.pid'
     pid_ok = False
     try:
         with pid.PidFile(pid_file, piddir=pid_dir) as p:
             pid_ok = True
-            DEFAULT_LOGGER.info(f'{SOFTWARE_VERSION} starting, PID:{p.pid}')
+            DEFAULT_LOGGER.info(f'{SOFTWARE_VERSION} starting, PID: {p.pid}, File: {pid_file_full}')
             DEFAULT_LOGGER.info(f'Config loaded from {CFG_FILE}:\n{yaml.dump(CONFIG, default_flow_style=False)}')
-
-            try:
-                host = CONFIG['rest']['listen_host']
-                port = CONFIG['rest']['listen_port']
-            except:
-                DEFAULT_LOGGER.critical(get_raised_error(full=True))
-                sys.exit(1)
 
             DEFAULT_LOGGER.info(f'Loading application modules...')
             for md in [os.path.basename(x)[:-3] for x in glob('./pkg/app/*.py') if x[-11:] != '__init__.py']:
@@ -48,4 +49,4 @@ if __name__ == '__main__':
         if pid_ok:
             raise
         else:
-            DEFAULT_LOGGER.critical(f'Something wrong with {pid_dir}/{pid_file}.pid. Maybe it\'s locked?')
+            DEFAULT_LOGGER.critical(f'Something wrong with {pid_file_full}. Maybe it\'s locked?')

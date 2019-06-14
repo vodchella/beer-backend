f600154eba224a29aa65703e6623834752f75d68
vodchella <twister.kz@gmail.com>
2019-06-14 14:48:34 +0600

    Добавил favicon

--- rest/pkg/app/routes/root/__init__.py
+++ rest/pkg/app/routes/root/__init__.py
@@ -4,7 +4,21 @@ from pkg.decorators import handle_exceptions
 from sanic import response
 
 
+#
+#  Главная страница
+#
+
+
 @app.get('/')
 @handle_exceptions
 async def ping(request):
     return response.json({'software': SOFTWARE_VERSION})
+
+
+#
+#  Статика
+#
+
+
+app.static('/favicon.png', './pkg/app/static/images/favicon.png')
+app.static('/favicon.ico', './pkg/app/static/images/favicon.ico')

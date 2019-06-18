2c17f943128766648a0c0a3a6fdfab9f15df2595
vodchella <twister.kz@gmail.com>
2019-06-18 22:08:52 +0600

    Комментарий по поводу коллизий

--- rest/pkg/utils/peewee.py
+++ rest/pkg/utils/peewee.py
@@ -14,4 +14,7 @@ def model_to_json(model):
 
 
 def generate_unique_id():
+    # ~4 million years needed (speed: 1000 IDs per hour),
+    # in order to have a 1% probability of at least one collision with current params
+    # https://zelark.github.io/nano-id-cc/
     return generate(ID_FIELD_ALPHABET, ID_FIELD_LENGTH)

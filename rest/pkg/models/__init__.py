7220b729c6db13bdf6c5f200766382f25e672e1e
vodchella <twister.kz@gmail.com>
2019-06-18 23:04:21 +0600

    Основные типы колонок БД выделены в отдельные классы

--- rest/pkg/models/__init__.py
+++ rest/pkg/models/__init__.py
@@ -4,12 +4,32 @@ from pkg.app import app
 from pkg.constants.database import ID_FIELD_LENGTH
 
 
+class PrimaryKeyCharField(FixedCharField):
+    def __init__(self, *args):
+        super(FixedCharField, self).__init__(*args, max_length=ID_FIELD_LENGTH, primary_key=True)
+
+
+class IsActiveField(BooleanField):
+    def __init__(self, *args):
+        super(BooleanField, self).__init__(*args, default=True, db_column='active_bool')
+
+
+class CreatedAtField(DateTimeField):
+    def __init__(self, *args):
+        super(DateTimeField, self).__init__(*args, default=datetime.now)
+
+
+#
+#  Сущности базы данных
+#
+
+
 class User(app.db.AsyncModel):
-    user_id = FixedCharField(max_length=ID_FIELD_LENGTH, primary_key=True)
+    user_id = PrimaryKeyCharField()
     email = TextField()
     password = TextField()
-    is_active = BooleanField(default=True, db_column='active_bool')
-    created_at = DateTimeField(default=datetime.now)
+    is_active = IsActiveField()
+    created_at = CreatedAtField()
 
     class Meta:
         schema = 'public'
@@ -17,10 +37,10 @@ class User(app.db.AsyncModel):
 
 
 class Company(app.db.AsyncModel):
-    company_id = FixedCharField(max_length=ID_FIELD_LENGTH, primary_key=True)
+    company_id = PrimaryKeyCharField()
     name = TextField()
-    is_active = BooleanField(default=True, db_column='active_bool')
-    created_at = DateTimeField(default=datetime.now)
+    is_active = IsActiveField()
+    created_at = CreatedAtField()
 
     class Meta:
         schema = 'public'

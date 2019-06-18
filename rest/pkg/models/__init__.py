88af662644195bfa0054e5cea1d2954f1726082a
vodchella <twister.kz@gmail.com>
2019-06-18 20:51:01 +0600

    Удалил поле User.seller_bool

--- rest/pkg/models/__init__.py
+++ rest/pkg/models/__init__.py
@@ -7,7 +7,6 @@ class User(app.db.AsyncModel):
     user_id = FixedCharField(max_length=16, primary_key=True)
     email = TextField()
     password = TextField()
-    seller_bool = BooleanField(default=False)
     active_bool = BooleanField(default=True)
     created_at = DateTimeField(default=datetime.now)
 

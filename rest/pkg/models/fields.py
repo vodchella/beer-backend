58f9277187a515048f5dee836640e6beaff2d232
vodchella <twister.kz@gmail.com>
2019-06-19 18:42:10 +0600

    Рефакторинг: объявления специфичных полей БД вынесены в pkg.models.fields

---
+++ rest/pkg/models/fields.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from peewee import *
+from pkg.constants.database import ID_FIELD_LENGTH
+
+
+class PrimaryKeyCharField(FixedCharField):
+    def __init__(self, *args):
+        super(FixedCharField, self).__init__(*args, max_length=ID_FIELD_LENGTH, primary_key=True)
+
+
+class ForeignKeyCharField(FixedCharField):
+    def __init__(self, rel_model, related_name=None, on_delete=None,
+                 on_update=None, extra=None, to_field=None,
+                 object_id_name=None, *args, **kwargs):
+        if rel_model != 'self' and not \
+                isinstance(rel_model, (Proxy, DeferredRelation)) and not \
+                issubclass(rel_model, Model):
+            raise TypeError('Unexpected value for `rel_model`.  Expected '
+                            '`Model`, `Proxy`, `DeferredRelation`, or "self"')
+        self.rel_model = rel_model
+        self._related_name = related_name
+        self.deferred = isinstance(rel_model, (Proxy, DeferredRelation))
+        self.on_delete = on_delete
+        self.on_update = on_update
+        self.extra = extra
+        self.to_field = to_field
+        self.object_id_name = object_id_name
+        super(ForeignKeyCharField, self).__init__(*args, max_length=ID_FIELD_LENGTH, **kwargs)
+
+
+class IsActiveField(BooleanField):
+    def __init__(self, *args):
+        super(BooleanField, self).__init__(*args, default=True, db_column='active_bool')
+
+
+class CreatedAtField(DateTimeField):
+    def __init__(self, *args):
+        super(DateTimeField, self).__init__(*args, default=datetime.now)

c5c7ad1f819908b3bd69f6d69837763c3afaa69d
vodchella <twister.kz@gmail.com>
2019-06-27 00:41:51 +0600

    Tests refactoring

--- rest/tests/behave/environment.py
+++ rest/tests/behave/environment.py
@@ -2,7 +2,8 @@ from tests.behave.utils import checked_request
 
 
 def before_all(context):
-    response = checked_request('get', 'http://localhost:8517/')
-    software = response.json()['software']
-    if not software.startswith('MyBeer REST server v'):
+    json = checked_request('get', 'http://localhost:8517/')
+    if 'software' not in json:
+        raise Exception('Invalid response format')
+    if not json['software'].startswith('MyBeer REST server v'):
         raise Exception('Invalid software version')
--- rest/tests/behave/utils/__init__.py
+++ rest/tests/behave/utils/__init__.py
@@ -1,4 +1,12 @@
+from json import JSONDecodeError
 from requests import HTTPError, request
+
+
+def get_response_json(response):
+    try:
+        return response.json()
+    except JSONDecodeError as e:
+        raise Exception(f'JSONDecodeError: {e}\nINVALID JSON: {response.text}')
 
 
 def checked_request(method, url, **kwargs):
@@ -6,6 +14,6 @@ def checked_request(method, url, **kwargs):
     try:
         response.raise_for_status()
     except HTTPError as e:
-        json = response.json()
+        json = get_response_json(response)
         raise Exception(f'{e}\nRESPONSE: {json}')
-    return response
+    return get_response_json(response)

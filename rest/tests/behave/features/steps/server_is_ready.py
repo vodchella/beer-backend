de1d9af70ad7d528aff1368a005fdcaf5e76fdbc
vodchella <twister.kz@gmail.com>
2019-06-26 21:48:22 +0600

    "Behave" test skeleton

---
+++ rest/tests/behave/features/steps/server_is_ready.py
@@ -0,0 +1,11 @@
+from behave import *
+
+
+@given('I send GET-request to http://localhost:8517/')
+def step_impl(context):
+    pass
+
+
+@then('I will see full software version')
+def step_impl(context):
+    pass

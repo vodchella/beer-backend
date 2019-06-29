a9bcff5faa91c2f4f9a5cc3eae56bc716368aa0c
vodchella <twister.kz@gmail.com>
2019-06-30 04:36:21 +0600

    Rename function

--- rest/pkg/app/routes/v1/users.py
+++ rest/pkg/app/routes/v1/users.py
@@ -12,7 +12,7 @@ USER_PATH = '/users/<user_id:[A-z0-9]+>'
 
 @v1.post(f'{USER_PATH}/change-password')
 @rest_context
-async def ping(context, user_id):
+async def change_password(context, user_id):
     body = context.request.json
     old_password = body.get('old', None)
     new_password = body.get('new', None)

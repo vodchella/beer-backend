ceca76f78a5419bbc38372149ce42927aa5b5c25
vodchella <twister.kz@gmail.com>
2019-06-30 04:52:57 +0600

    Refactoring

--- rest/pkg/config/__init__.py
+++ rest/pkg/config/__init__.py
@@ -4,7 +4,7 @@ from pkg.utils.console import panic
 from pkg.utils.files import read_file
 
 try:
-    CFG_FILE = (os.environ['BEER_REST_CONFIG'] if 'BEER_REST_CONFIG' in os.environ else None) or 'config.yml'
+    CFG_FILE = os.environ.get('BEER_REST_CONFIG', None) or 'config.yml'
     CONFIG = yaml.load(read_file(CFG_FILE), Loader=yaml.BaseLoader)
 except FileNotFoundError:
     panic()

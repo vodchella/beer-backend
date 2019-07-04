fc3b475751233b6db0b718db37c6ff2d43825ebd
vodchella <twister.kz@gmail.com>
2019-07-04 17:02:17 +0600

    Remove unused proc

--- rest/pkg/utils/peewee.py
+++ rest/pkg/utils/peewee.py
@@ -3,11 +3,6 @@ from pkg.constants.database import ID_FIELD_ALPHABET, ID_FIELD_LENGTH
 from playhouse.shortcuts import model_to_dict
 
 
-def fetch_one(async_result):
-    result = list(async_result)
-    return result[0] if len(result) else None
-
-
 def model_to_json(model):
     if model:
         return model_to_dict(model, exclude='aio')

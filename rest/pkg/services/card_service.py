76dd170433e0d00526b00f4e9000691365365ffb
vodchella <twister.kz@gmail.com>
2019-07-08 18:49:58 +0600

    Generate card number

--- rest/pkg/services/card_service.py
+++ rest/pkg/services/card_service.py
@@ -1,7 +1,16 @@
+from nanoid import generate
+from pkg.constants.database import CARD_NUMBER_LENGTH, CARD_NUMBER_ALPHABET
 from pkg.models import Card
+from pkg.utils.peewee import generate_unique_id
 
 
 class CardService:
     @staticmethod
+    def generate_number():
+        return generate(CARD_NUMBER_ALPHABET, CARD_NUMBER_LENGTH)
+
+    @staticmethod
     async def create():
-        return await Card.aio.create(card_id='test-identifier')
+        return await Card.aio.create(card_id=generate_unique_id(),
+                                     card_number=CardService.generate_number(),
+                                     )

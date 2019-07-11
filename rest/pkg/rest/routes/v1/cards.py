83fd3c30903d59c423081246b57a9248b32864d6
vodchella <twister.kz@gmail.com>
2019-07-11 21:10:38 +0600

    Check for employee.company_id equals card.company_id

--- rest/pkg/rest/routes/v1/cards.py
+++ rest/pkg/rest/routes/v1/cards.py
@@ -3,7 +3,7 @@ from pkg.constants.error_codes import *
 from pkg.decorators import employee_rest_context
 from pkg.services.card_service import CardService
 from pkg.services.user_service import UserService
-from pkg.utils.errors import response_400, response_404, response_error
+from pkg.utils.errors import response_400, response_403, response_404, response_error
 from pkg.utils.peewee import model_to_json
 from sanic import response
 
@@ -34,30 +34,33 @@ async def accumulate_value(context, card_id):
     if card:
         if card.type_of_card == 'accumulation':
             if card.is_active:
-                body = context.request.parsed_json
-                if body:
-                    increase_by = body.get('increase_by', None)
-                    if increase_by and increase_by > 0:
-                        new_value = card.attributes['value'] + increase_by
+                if context.employee.company_id == card.company_id:
+                    body = context.request.parsed_json
+                    if body:
+                        increase_by = body.get('increase_by', None)
+                        if increase_by and increase_by > 0:
+                            new_value = card.attributes['value'] + increase_by
 
-                        limit = card.attributes.get('limit', 0)
-                        if limit > 0:
-                            if new_value > limit:
-                                return response_error(ERROR_CARD_LIMIT_EXCEEDED)
-                            elif new_value == limit:
-                                card.is_active = False
+                            limit = card.attributes.get('limit', 0)
+                            if limit > 0:
+                                if new_value > limit:
+                                    return response_error(ERROR_CARD_LIMIT_EXCEEDED)
+                                elif new_value == limit:
+                                    card.is_active = False
 
-                        card.attributes['value'] = new_value
-                        await CardService.update(card)
+                            card.attributes['value'] = new_value
+                            await CardService.update(card)
 
-                        response_json = {'result': model_to_json(card)}
-                        if not card.is_active:
-                            response_json['message'] = 'Card was deactivated because of fullfilled'
-                        return response.json(response_json)
+                            response_json = {'result': model_to_json(card)}
+                            if not card.is_active:
+                                response_json['message'] = 'Card was deactivated because of fullfilled'
+                            return response.json(response_json)
+                        else:
+                            return response_400(context.request)
                     else:
-                        return response_400(context.request)
+                        return response_error(ERROR_JSON_PARSING_EXCEPTION)
                 else:
-                    return response_error(ERROR_JSON_PARSING_EXCEPTION)
+                    return response_403(context.request)
             else:
                 return response_error(ERROR_CARD_IS_NOT_ACTIVE)
         else:

81e8da2e0129e250d6a54c9bc15978e012afca81
vodchella <twister.kz@gmail.com>
2019-07-15 22:20:26 +0600

    Refactoring

--- rest/pkg/services/card_service.py
+++ rest/pkg/services/card_service.py
@@ -14,18 +14,19 @@ class CardService:
     @staticmethod
     async def create(issuer: Employee, owner: User, name: str):
         company = await CompanyService.find_by_service_point_id(issuer.service_point_id)
-        return await Card.aio.create(card_id=generate_unique_id(),
-                                     card_number=CardService.generate_number(),
-                                     type_of_card='accumulation',
-                                     company_id=company.company_id,
-                                     owner_id=owner.user_id,
-                                     issuer_id=issuer.employee_id,
-                                     issued_in_service_point_id=issuer.service_point_id,
-                                     attributes={
-                                         'name': name,
-                                         'limit': 19,
-                                         'value': 0,
-                                     })
+        card_id = await app.db.aio.insert(Card.insert(card_id=generate_unique_id(),
+                                                      card_number=CardService.generate_number(),
+                                                      type_of_card='accumulation',
+                                                      company_id=company.company_id,
+                                                      owner_id=owner.user_id,
+                                                      issuer_id=issuer.employee_id,
+                                                      issued_in_service_point_id=issuer.service_point_id,
+                                                      attributes={
+                                                          'name': name,
+                                                          'limit': 19,
+                                                          'value': 0,
+                                                      }))
+        return await CardService.find(card_id)
 
     @staticmethod
     async def find(card_id):

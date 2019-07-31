ddb48607a152930e76c778701580cd3543358a65
vodchella <twister.kz@gmail.com>
2019-07-31 23:45:41 +0600

    Tests for accumulating and fullfilling card

--- rest/tests/behave/features/accumulate_card.feature
+++ rest/tests/behave/features/accumulate_card.feature
@@ -7,3 +7,11 @@ Feature: Accumulate card
   Scenario: Specify invalid card ID
       Given I try to accumulate card with invalid card ID
        Then I will get "404" http error
+
+  Scenario: Really trying to accumulate card
+      Given I send correct data to accumulate card
+       Then I will get Ok http status and modified card value
+
+  Scenario: Trying to fulfill card
+      Given I send data to fulfill card
+       Then I will get Ok http status and disabled card with info message
--- rest/tests/behave/features/steps/accumulate_card.py
+++ rest/tests/behave/features/steps/accumulate_card.py
@@ -3,6 +3,13 @@ from tests.behave.utils.constants import CARDS_PATH
 from tests.behave.utils import behave_request, authorized_behave_request
 
 
+def create_new_card():
+    url = f'{CARDS_PATH}/create'
+    payload = '{"owner_id": "DaNhiRv862lsVbGx", "name": "Test accumulation card", "type": "accumulation"}'
+    response = authorized_behave_request('POST', url, data=payload)
+    return response.json()['result']
+
+
 @given("I try to accumulate card with invalid bearer token")
 def step_impl(context):
     url = f'{CARDS_PATH}/HY3jBpIsGIWJ6fdj/accumulate'
@@ -13,3 +20,48 @@ def step_impl(context):
 def step_impl(context):
     url = f'{CARDS_PATH}/invalid-id/accumulate'
     context.response = authorized_behave_request('POST', url)
+
+
+@given("I send correct data to accumulate card")
+def step_impl(context):
+    new_card_id = create_new_card()['card_id']
+    url = f'{CARDS_PATH}/{new_card_id}/accumulate'
+    payload = '{"increase_by": 2}'
+    context.response = authorized_behave_request('POST', url, data=payload)
+
+
+@then("I will get Ok http status and modified card value")
+def step_impl(context):
+    assert context.response.status_code == 200
+    json_ = context.response.json()
+    assert 'result' in json_
+    result = json_['result']
+    assert 'attributes' in result
+    attributes = result['attributes']
+    assert 'value' in attributes
+    value = attributes['value']
+    assert value == 2
+    assert 'is_active' in result
+    is_active = result['is_active']
+    assert is_active is True
+
+
+@given("I send data to fulfill card")
+def step_impl(context):
+    new_card = create_new_card()
+    new_card_id = new_card['card_id']
+    limit = new_card['attributes']['limit']
+    url = f'{CARDS_PATH}/{new_card_id}/accumulate'
+    payload = '{"increase_by": ' + str(limit) + '}'
+    context.response = authorized_behave_request('POST', url, data=payload)
+
+
+@then("I will get Ok http status and disabled card with info message")
+def step_impl(context):
+    assert context.response.status_code == 200
+    json_ = context.response.json()
+    assert 'result' in json_
+    result = json_['result']
+    assert 'is_active' in result
+    is_active = result['is_active']
+    assert is_active is False

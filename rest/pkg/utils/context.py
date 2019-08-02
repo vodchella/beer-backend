6bbe4de20f0d515c30f4fe035c1d08a887e4eeca
vodchella <twister.kz@gmail.com>
2019-08-02 19:08:12 +0600

    Type annotations in ServerContext

--- rest/pkg/utils/context.py
+++ rest/pkg/utils/context.py
@@ -1,7 +1,22 @@
 import inspect
 
+from lib.sanic_peewee import QueryHandlerMixin
+from pkg.models import User, Employee
+from sanic.request import Request
 
-def get_current_context():
+
+class ServerContext:
+    db: QueryHandlerMixin.aio = None
+    request: Request = None
+    user: User = None
+    employee: Employee = None
+    json_body: dict = None
+
+    def __init__(self):
+        pass
+
+
+def get_current_context() -> ServerContext:
     stack = inspect.stack()
     for frame_info in stack:
         frame_vars = frame_info.frame.f_locals
@@ -13,13 +28,3 @@ def get_current_context():
                         ctx = value._app_context
                         if type(ctx) == ServerContext:
                             return ctx
-
-
-class ServerContext:
-    db = None
-    request = None
-    user = None
-    employee = None
-
-    def __init__(self):
-        pass

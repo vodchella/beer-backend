bbb2ec0be433dc6adb45b95ac1ba938d9cc8420a
vodchella <twister.kz@gmail.com>
2019-08-29 23:36:02 +0600

    Exception in Behave when can't connect to server

--- rest/tests/behave/features/environment.py
+++ rest/tests/behave/features/environment.py
@@ -4,7 +4,10 @@ from tests.behave.utils import checked_behave_request
 
 # noinspection PyUnusedLocal
 def before_all(context: Context):
-    json = checked_behave_request('GET', 'http://localhost:8517/')
+    try:
+        json = checked_behave_request('GET', 'http://localhost:8517/')
+    except:
+        raise Exception('Can\'t connect to server')
     if 'software' not in json:
         raise Exception('Invalid response format')
     if not json['software'].startswith('MyBeer REST server v'):
